358c9bacc92c1e087ffb4afdfc71257acb1a5213
Michael Scheuchenpflug <michaelscheuchenpflug@gmx.at>
2019-10-10 09:58:06 +0200

    finished, I know It's late, but I had some mystical malfunction

---
+++ AsciiArtCombiner/AsciiArtCombiner/Program.cs
@@ -0,0 +1,39 @@
+﻿using AsciiArtCombinerLibrary;
+using System;
+using System.Collections;
+using System.IO;
+
+namespace AsciiArtCombiner
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("You need to specify at least 2 files!");
+                return;
+            }
+
+            string[] textFiles = new string[args.Length];
+
+            readFiles(textFiles, args);
+
+            AsciiCombiner combiner = new AsciiCombiner();
+            string result = combiner.combineTexts(textFiles);
+            Console.WriteLine("\n" + result);
+        }
+
+        private static void readFiles(string[] textFiles, string[] fileNames)
+        {
+            for(int i = 0; i < fileNames.Length; i++)
+            {
+                textFiles[i] = File.ReadAllText(fileNames[i]);
+            }
+        }
+    }
+}
---
+++ AsciiArtCombiner/AsciiArtCombinerLibrary/AsciiCombiner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AsciiArtCombinerLibrary
+{
+    public class AsciiCombiner
+    {
+        string result = "";
+
+        public string combineTexts(IEnumerable<string> texts)
+        {
+            checkTextCompatability(texts);
+
+            foreach (string text in texts)
+            {
+                addLayer(text);
+            }
+            return result;
+        }
+
+        public bool checkTextCompatability(IEnumerable<string> texts)
+        {
+            int columns = -1;
+            int rows = -1;
+
+            foreach(string text in texts)
+            {
+                string[] lines = text.Replace("\r", "").Split('\n');
+                if (rows > -1 && lines.Length != rows) return false;
+                else if (rows == -1) rows = lines.Length;
+
+                foreach(string line in lines)
+                {
+                    if (columns > -1 && line.Length != columns) return false;
+                    else if (columns == -1) columns = line.Length;
+                }
+            }
+
+            return true;
+        }
+
+        private void addLayer(string text)
+        {
+            if (result.Equals(""))
+            {
+                result = text;
+                return;
+            }
+
+            string[] newLines = text.Replace("\r", "").Split('\n');
+            string[] prevResultLines = result.Replace("\r", "").Split('\n');
+
+            string[] newResult = (string[])prevResultLines.Clone();
+
+            for (int i = 0; i<newLines.Length; i++)
+            {
+                newResult[i] = "";
+                for(int j = 0; j<newLines[i].Length; j++)
+                {
+                    if (newLines[i][j] != ' ') newResult[i] += newLines[i][j];
+                    else newResult[i] += prevResultLines[i][j];
+                }
+            }
+            string newCombinedResult = String.Join("\n", newResult);
+            string[] temp = { result, newCombinedResult };
+            if (!checkTextCompatability(temp)){
+                Console.WriteLine("ERROR: Result is not compatible with new Result!");
+            }
+            else
+            {
+                result = newCombinedResult;
+            }
+        }
+    }
+}
